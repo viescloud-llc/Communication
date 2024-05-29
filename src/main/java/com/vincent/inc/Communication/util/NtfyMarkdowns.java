d80d4916306b8da85bcbedbcd9030f3d809c1469
VincentB00 <vincentbui1999@gmail.com>
2024-05-29 13:25:30 -0400

    add ntfy markdown utils

--- src/main/java/com/vincent/inc/Communication/util/NtfyMarkdowns.java
+++ src/main/java/com/vincent/inc/Communication/util/NtfyMarkdowns.java
@@ -1,14 +1,55 @@
 package com.vincent.inc.Communication.util;
 
+import java.util.Collections;
+
 public class NtfyMarkdowns {
     private NtfyMarkdowns() {}
 
-    public static final String BOLD = "bold";
-    public static final String ITALIC = "italic";
-    public static final String STRIKE = "strike";
-    public static final String CODE = "code";
-    public static final String CODE_BLOCK = "code-block";
-    public static final String QUOTE = "quote";
-    public static final String LINK = "link";
-    public static final String IMAGE = "image";
+    public static String bold(String text) {
+        return String.format("**%s**", text);
+    }
+
+    public static String italic(String text) {
+        return String.format("*%s*", text);
+    }
+
+    public static String strikethrough(String text) {
+        return String.format("~~%s~~", text);
+    }
+
+    public static String inlineCode(String text) {
+        return String.format("`%s`", text);
+    }
+
+    public static String link(String text, String url) {
+        return String.format("[%s](%s)", text, url);
+    }
+
+    public static String image(String text, String url) {
+        return String.format("![](%s) %s", url, text);
+    }
+
+    public static String blockQuote(String text) {
+        return String.format("> %s", text);
+    }
+
+    public static String codeBlock(String text) {
+        return String.format("```\n%s\n```", text);
+    }
+
+    public static String header(String text, int level) {
+        return String.format("%s %s", String.join("", Collections.nCopies(level, "#")), text);
+    }
+
+    public static String unorderedList(String text) {
+        return String.format("- %s", text);
+    }
+
+    public static String orderedList(String text) {
+        return String.format("1. %s", text);
+    }
+
+    public static String horizontalRule() {
+        return "---";
+    }
 }
