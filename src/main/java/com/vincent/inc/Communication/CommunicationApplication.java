553fce744d9d49680e98c2c9d6c9f2b18e59cf52
VincentB00 <vincentbui1999@gmail.com>
2024-05-28 23:48:49 -0400

    update vies lib

--- src/main/java/com/vincent/inc/Communication/CommunicationApplication.java
+++ src/main/java/com/vincent/inc/Communication/CommunicationApplication.java
@@ -2,12 +2,25 @@ package com.vincent.inc.Communication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vincent.inc.viesspringutils.ViesApplication;
 
 @SpringBootApplication
-public class CommunicationApplication {
+@EnableDiscoveryClient
+@RestController
+@EnableFeignClients
+public class CommunicationApplication extends ViesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommunicationApplication.class, args);
+	}
+
+	@Override
+	public String getApplicationName() {
+		return "Communication";
 	}
 
 }
