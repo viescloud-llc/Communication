d5f47ed9cc679afbcc89c20c43fa59c100b6fe89
VincentB00 <vincentbui1999@gmail.com>
2025-02-27 09:41:01 -0500

    add service annotation

--- src/main/java/com/vincent/inc/Communication/service/NtfyTopicService.java
+++ src/main/java/com/vincent/inc/Communication/service/NtfyTopicService.java
@@ -2,9 +2,13 @@ package com.vincent.inc.Communication.service;
 
 import com.vincent.inc.Communication.dao.NtfyTopicDao;
 import com.vincent.inc.Communication.model.ntfy.NtfyTopic;
+
+import org.springframework.stereotype.Service;
+
 import com.viescloud.llc.viesspringutils.repository.DatabaseCall;
 import com.viescloud.llc.viesspringutils.service.ViesService;
 
+@Service
 public class NtfyTopicService extends ViesService<Integer, NtfyTopic, NtfyTopicDao> {
 
     public NtfyTopicService(DatabaseCall<Integer, NtfyTopic> databaseCall, NtfyTopicDao repositoryDao) {
