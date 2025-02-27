05de2ae5e798aeb68ef8f2349b393c0fb14bb90b
VincentB00 <vincentbui1999@gmail.com>
2025-02-27 14:23:17 -0500

    update email provider controller

--- src/main/java/com/vincent/inc/Communication/controller/EmailProviderController.java
+++ src/main/java/com/vincent/inc/Communication/controller/EmailProviderController.java
@@ -3,13 +3,13 @@ package com.vincent.inc.Communication.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.viescloud.llc.viesspringutils.controller.ViesController;
+import com.viescloud.llc.viesspringutils.controller.ViesControllerWithUserAccess;
 import com.vincent.inc.Communication.model.email.EmailProvider;
 import com.vincent.inc.Communication.service.EmailProviderService;
 
 @RestController
 @RequestMapping("/email/providers")
-public class EmailProviderController extends ViesController<Integer, EmailProvider, EmailProviderService> {
+public class EmailProviderController extends ViesControllerWithUserAccess<Integer, EmailProvider, EmailProviderService> {
 
     public EmailProviderController(EmailProviderService service) {
         super(service);
